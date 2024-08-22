09e00a272b5a56a0f4a594fa29cc7c99c61f011a
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-08-22 15:24:56 +0300

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import Contact from "./pages/Contact"
 import Product from "./pages/Product"
 import PlaceOrder from "./pages/PlaceOrder"
 import Orders from "./pages/Orders"
+import Navbar from "./components/Navbar"
 
 
 function App() {
   
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/collection" element={<Collection />}/>
--- src/assets/images/assets.js
+++ src/assets/images/assets.js
@@ -1,5 +1,8 @@
 import logo from './logo.png'
 import add_icon from './add_icon.png'
+import cart_icon from './cart_icon.png'
+import profile_icon from './profile_icon.png'
+import search_icon from './search_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
@@ -9,6 +12,10 @@ export const assets = {
     add_icon,
     order_icon,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    cart_icon,
+    profile_icon,
+    search_icon
+
 }
 
