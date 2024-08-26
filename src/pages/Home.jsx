71c79459017e7420bf65604ee531ebaf0451f304
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-08-26 12:52:55 +0300

    add policy section and subscribe form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "../components/Hero";
 import LatestCollections from "../components/LatestCollections";
 import BestSeller from "../components/BestSeller";
+import Policy from "../components/Policy";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 function Home() {
   return (
@@ -9,6 +11,8 @@ function Home() {
       <Hero />
       <LatestCollections />
       <BestSeller />
+      <Policy />
+      <NewsLetterBox />
     </>
   );
 }
