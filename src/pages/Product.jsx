e0d9c3b0a26ca7c0915ba992a3df51e55ff64ec6
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-08-30 15:03:44 +0300

    Design Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/frontend_assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 function Product() {
-  return (
-    <div>Product</div>
-  )
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [products, productId]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* Product Data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product Images */}
+        <div className="flex-1 flex flex-col-reverse gap-5 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                alt=""
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} className="w-full h-auto" alt="" />
+          </div>
+        </div>
+        {/* --------------- product info --------------- */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2 font-prata">
+            {productData.name}
+          </h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_dull_icon} alt="" className="w-3" />
+            <p className="pl-2 text-gray-500">(122)</p>
+          </div>
+          <p className="font-medium font-prata text-xl mt-5">
+            {currency} {productData.price}
+          </p>
+          <p className="text-gray-600 mt-5 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  className={`border py-2 px-4 rounded bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                  onClick={() => setSize(item)}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white text-sm py-3 px-8 rounded active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product.</p>
+            <p>Cash on delivery is avilable on this product.</p>
+            <p>Easy return and exchange police within 7 days.</p>
+          </div>
+        </div>
+      </div>
+      {/* --------------Descriptions and review section ------------ */}
+      <div className="mt-8">
+        <div className="flex">
+          <b className="border  px-5 py-3 text-sm bg-gray-100">Description</b>
+          <p className="border  px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus,
+            deserunt blanditiis reiciendis voluptatibus eum libero corporis
+            velit dolore veniam sapiente eaque recusandae enim repudiandae
+            necessitatibus quo? Adipisci, harum? Eius, at.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            veritatis dicta corporis? Alias quos assumenda impedit aspernatur
+            animi ipsam sint harum eos eveniet voluptates. Aperiam ipsa magnam
+            quis voluptate architecto.
+          </p>
+        </div>
+      </div>
+
+      {/* ------------ Display related products --------------*/}
+      <div>
+        <RelatedProducts
+          category={productData.category}
+          subCategory={productData.subCategory}
+        />
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 }
 
-export default Product
+export default Product;
